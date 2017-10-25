7f073c2eeedbd1afadcde0ce5d0c6868505c4130
Daren Desjardins <darend@gmail.com>
2017-10-25 12:17:42 -0700

    Format

--- output.tf
+++ output.tf
@@ -1,20 +1,19 @@
-
 output "vpc_id" {
-    value = "module.vpc.vpc_id"
+  value = "module.vpc.vpc_id"
 }
 
 output "availability_zone" {
-    value = "${aws_db_instance.replica.availability_zone}"
+  value = "${aws_db_instance.replica.availability_zone}"
 }
 
 output "replica_arn" {
-    value = "${aws_db_instance.replica.arn}"
+  value = "${aws_db_instance.replica.arn}"
 }
 
 output "replica_address" {
-    value = "${aws_db_instance.replica.address}"
+  value = "${aws_db_instance.replica.address}"
 }
 
 output "replica_endpoint" {
-    value = "${aws_db_instance.replica.endpoint}"
+  value = "${aws_db_instance.replica.endpoint}"
 }
