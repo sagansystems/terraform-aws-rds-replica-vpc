0c3898baea047edfa5276864c924fee4bd1e704a
Daren Desjardins <darend@gmail.com>
2018-02-26 14:46:17 -0800

    Use join

--- output.tf
+++ output.tf
@@ -3,17 +3,17 @@ output "vpc_id" {
 }
 
 output "availability_zone" {
-  value = "${aws_db_instance.replica.*.availability_zone}"
+  value = "${join("", aws_db_instance.replica.*.availability_zone)}"
 }
 
 output "replica_arn" {
-  value = "${aws_db_instance.replica.*.arn}"
+  value = "${join("", aws_db_instance.replica.*.arn)}"
 }
 
 output "replica_address" {
-  value = "${aws_db_instance.replica.*.address}"
+  value = "${join("", aws_db_instance.replica.*.address)}"
 }
 
 output "replica_endpoint" {
-  value = "${aws_db_instance.replica.*.endpoint}"
+  value = "${join("", aws_db_instance.replica.*.endpoint)}"
 }
